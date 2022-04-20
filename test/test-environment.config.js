8c118945b242988fe20a376a7a8e8c64328cb3f6
sacru2red <sacru2red@gmail.com>
2022-04-20 21:20:57 +0900

    add test config file

---
+++ test/test-environment.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    accounts: {
+        amount: 10,
+        ether: 100,
+    },
+    contracts: {
+        type: 'truffle',
+        defaultGas: 6e6,
+    },
+    blockGasLimit: 8e6
+}
